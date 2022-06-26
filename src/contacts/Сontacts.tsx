a5e31aaa584b2b3128de3ca4695c58046a7e1ec5
Andrey <fail4iks@gmail.com>
2022-06-26 23:08:00 +0300

    Edit contact module

--- src/contacts/Сontacts.tsx
+++ src/contacts/Сontacts.tsx
@@ -1,15 +1,21 @@
-import style from './Сontacts.module.css'
+import style from './Сontacts.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
+import {Title} from "../common/components/title/Title";
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <h2 className={style.title}>Contacts</h2>
+                <Title title={'Hire Me.'}
+                       subTitle={'Let\'s Say Hi'}
+                       description={'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.'}/>
+                <p className={style.email}>
+                    Contact Email: <a className={style.imHover} href="mailto:example@gmail.com">example@gmail.com</a>
+                </p>
                 <form className={style.form}>
-                    <input placeholder="Your name" type="text" name="name"/>
-                    <input placeholder="Your e-mail" type="text" name="email"/>
-                    <textarea placeholder="Enter your message" name="message"/>
+                    <input placeholder="Your name" type="text" name="name" className={style.input}/>
+                    <input placeholder="Your e-mail" type="text" name="email" className={style.input}/>
+                    <textarea placeholder="Enter your message" name="message" className={style.textarea}/>
                 </form>
                 <button className={style.button}>Send</button>
 
