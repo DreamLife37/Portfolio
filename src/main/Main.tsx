7d7ec772998f8c967bd6059c3ca367bd3fadcfa2
Andrey <fail4iks@gmail.com>
2022-07-18 22:49:28 +0300

    Add file CV

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,5 +1,7 @@
 import style from './Main.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
+// @ts-ignore
+import cv from '../assets/files/cv.pdf'
 
 export const Main = () => {
     return (
@@ -12,7 +14,8 @@ export const Main = () => {
                         <span>Frontend Developer</span>
                     </h1>
 
-                    <a className={style.downloadCV} href={'ya.ru'}>DOWNLOAD CV</a>
+                    <a href={cv} className={style.downloadCV} target={"_blank"} rel="noopener noreferrer">DOWNLOAD
+                        CV</a>
 
                 </div>
                 <div className={style.photo}></div>
