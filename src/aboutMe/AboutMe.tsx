62b30daa8159c0141054710d3fab0235b9f6a34a
Andrey <fail4iks@gmail.com>
2022-08-20 11:14:53 +0300

    Added Typing Effect

--- src/aboutMe/AboutMe.tsx
+++ src/aboutMe/AboutMe.tsx
@@ -16,39 +16,36 @@ export const AboutMe = () => {
     return (
 
         <div className={style.aboutMeBlock}>
-            <Fade direction={"up"} triggerOnce duration={1000}>
+            <Fade delay={500} triggerOnce duration={1000}>
                 <div className={`${styleContainer.container} ${style.aboutMeContainer}`}>
-                    <div className={style.aboutMe}>
-                        <div className={style.aboutMeWrapper}><Title title={'About Me'}
-                                                                     subTitle={'My About Details'}
-                                                                     description={'I\'m a results-oriented frontend developer.\n' +
-                                                                         'I\'m learning, developing, constantly improving my skills, I want to participate in cool projects.'}/>
+                    <Title title={'About Me'}
+                           subTitle={'My About Details'}
+                           description={'I\'m a results-oriented frontend developer.\n' +
+                               'I\'m learning, developing, constantly improving my skills'}/>
+                    <div className={style.menuTab}>
+                        <div className={style.navMenu}>
+                            <a onClick={() => setCurrentTab('MAIN_SKILLS')}
+                               className={currentTab === 'MAIN_SKILLS' ? style.navItemActive : style.navItem}>Main
+                                skills</a>
                         </div>
-                        <div className={style.menuTab}>
-                            <div className={style.navMenu}>
-                                <a onClick={() => setCurrentTab('MAIN_SKILLS')}
-                                   className={currentTab === 'MAIN_SKILLS' ? style.navItemActive : style.navItem}>Main
-                                    skills</a>
-                            </div>
-                            <div className={style.navMenu}>
-                                <a onClick={() => setCurrentTab('EDUCATION')}
-                                   className={currentTab === 'EDUCATION' ? style.navItemActive : style.navItem}>Education</a>
-                            </div>
-                            <div className={style.navMenu}>
-                                <a onClick={() => setCurrentTab('WORK_EXPERIENCE')}
-                                   className={currentTab === 'WORK_EXPERIENCE' ? style.navItemActive : style.navItem}>Work
-                                    experience</a>
-                            </div>
+                        <div className={style.navMenu}>
+                            <a onClick={() => setCurrentTab('EDUCATION')}
+                               className={currentTab === 'EDUCATION' ? style.navItemActive : style.navItem}>Education</a>
                         </div>
-
-                        <div className={style.contentTab}>
-                            {(currentTab === 'MAIN_SKILLS') &&
-                                <Skills activeTab/>
-                            }
-                            {(currentTab === 'EDUCATION') &&
-                                <Education/>
-                            }</div>
+                        <div className={style.navMenu}>
+                            <a onClick={() => setCurrentTab('WORK_EXPERIENCE')}
+                               className={currentTab === 'WORK_EXPERIENCE' ? style.navItemActive : style.navItem}>Work
+                                experience</a>
+                        </div>
                     </div>
+                    <div className={style.contentTab}>
+                        {(currentTab === 'MAIN_SKILLS') &&
+                            <Skills activeTab/>
+                        }
+                        {(currentTab === 'EDUCATION') &&
+                            <Education/>
+                        }</div>
+
                 </div>
             </Fade>
         </div>
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -3,24 +3,32 @@ import styleContainer from './../common/styles/Container.module.css'
 // @ts-ignore
 import cv from '../assets/files/cv.pdf'
 import photo from '../assets/images/photo.jpg'
+import {Fade} from 'react-awesome-reveal'
+import ReactTypingEffect from 'react-typing-effect'
+
 
 export const Main = () => {
     return (
-        <div className={style.mainBlock}>
-            <div className={style.container}>
-                <div className={style.titleBlock}>
-                    <span className={style.subTitle}>Hi</span>
-                    <h1 className={style.title}>I'm Andrey Schavelev
-                        <br/>
-                        <span>Frontend Developer</span>
-                    </h1>
-                    <a href={cv} className={style.downloadCV} target={"_blank"} rel="noopener noreferrer">DOWNLOAD
-                        CV</a>
+
+            <div className={style.mainBlock}>
+                <Fade delay={100} triggerOnce >
+                <div className={` ${style.mainContainer}`}>
+                    <div className={style.titleBlock}>
+                        <span className={style.subTitle}>Hi</span>
+                        <h1 className={style.title}>I'm Andrey Schavelev
+                            <br/>
+                            <ReactTypingEffect
+                                text={'Frontend Developer'}
+                            />
+                        </h1>
+                        <a href={cv} className={style.downloadCV} target={"_blank"} rel="noopener noreferrer">DOWNLOAD
+                            CV</a>
+                    </div>
+                    <img className={style.photo} src={photo}/>
                 </div>
-                <img className={style.photo} src={photo}/>
+
+                </Fade>
             </div>
 
-
-        </div>
     )
 }
