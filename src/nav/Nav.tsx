e7bf2ae79e2cef6c14cd8f31ac60f64a270e3630
Andrey <fail4iks@gmail.com>
2022-08-20 12:27:04 +0300

    Delete .idea

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -5,7 +5,7 @@ export const Nav = () => {
         <div className={style.navSection}>
             <div className={style.navItem}><a href="">Main</a></div>
             <div className={style.navItem}><a href="">About Me</a></div>
-            <div className={style.navItem}><a href="">My projects</a></div>
+            <div className={style.navItem}><a href="#projects">My projects</a></div>
             <div className={style.navItem}><a href="">Contacts</a></div>
         </div>
     )
