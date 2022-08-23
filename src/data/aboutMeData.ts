ad9be71d844aac768cbe3f8397e3ebb3d2a2af69
Andrey <fail4iks@gmail.com>
2022-08-23 21:36:43 +0300

    Added a translation of the work experience module

--- src/data/aboutMeData.ts
+++ src/data/aboutMeData.ts
@@ -5,7 +5,24 @@ export const aboutMeData = {
         description: 'Я фронтенд-разработчик, с опытом создания SPA, используя React, Redux, TypeScript.\n' +
             'Учусь, развиваюсь, постоянно совершенствую свои навыки, хочу участвовать в крутых проектах. \n' +
             'В будущем планирую стать full-stack разработчиком.',
-        nameTabs: ['Главные навыки', 'Образование', 'Опыт работы']
+        nameTabs: ['Главные навыки', 'Образование', 'Опыт работы'],
+        workExperience: {
+            frontEnd: {
+                title: 'Мои работы как Front-end разработчика:',
+                projectCards: 'Проект: Обучающие карточки',
+                projectSocialNetwork: 'Проект: Социальная сеть',
+                projectTodolist: 'Проект: Список дел',
+                projectRestaurant: 'Проект: Приложение для ресторана',
+                projectElektronika24: 'Проект: Электроника24',
+                projectServisComp: 'Сайт компании “Сервис Компьютер”',
+                description: 'Создание сайта на WordPress, его администрирование, продвижение',
+            },
+            serviceEngineer: {
+                title: 'Сервисный инженер:',
+                placeWork: 'Компания: Сервис Компьютер',
+                description: 'Специалист по установке, настройке, поиску неисправностей и ремонту высокотехнологичной техники.'
+            }
+        }
     },
 
     en: {
@@ -14,6 +31,23 @@ export const aboutMeData = {
         description: 'I\'m a results-oriented front-end developer.\n' +
             'I\'m learning, developing, constantly improving my skills',
         nameTabs: ['Main skills', 'Education', 'Work experience'],
+        workExperience: {
+            frontEnd: {
+                title: 'My work as a Front-end developer:',
+                projectCards: 'Project: Learning Cards',
+                projectSocialNetwork: 'Project: Social Network',
+                projectTodolist: 'Project: Todolist',
+                projectRestaurant: 'Project: Restaurant app',
+                projectElektronika24: 'Project: Elektronika24',
+                projectServisComp: 'The website of the company "Service Computer”',
+                description: 'WordPress website creation, administration, promotion',
+            },
+            serviceEngineer: {
+                title: 'Service Engineer:',
+                placeWork: 'Company: Service Computer',
+                description: 'Specialist in the installation, setup, troubleshooting and repair of high-tech equipment'
+            }
+        }
     },
 
 }
